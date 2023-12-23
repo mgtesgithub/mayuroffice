d1c75c35b0d5d03e978639d25c30886bcdb433ce
Mayur Patil <mp903056@gmail.com>
2023-12-23 16:25:38 +0530

    Today changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  
+  const[data,setdata] = useState();
+  const[clickfunc,setclickfunc] = useState();
+
+  function changedata (event){
+    setdata(event.target.value)
+
+  }
+
+  function onclick(){
+    setclickfunc(data)
+  }
+
+
+  // function getdata(event){
+  //   setdata(event.target.value);
+  //   setprint(false)
+  // }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <h2>Mayur Patil</h2>
-      </header>
-    </div>
+  
+     <div className="App">
+      {/* {print ? <h1>{data}</h1> : null}
+      <input type="text"   onChange={getdata} />
+      <button onClick={()=>setprint(true)}>Print data</button> */}
+
+
+        <input type="text" value={data}  onChange={changedata}/>
+        <br /><br />
+        <button className="btn btn-primary" onClick={()=>onclick()}>click</button><br /><br />
+        <input type="text"  value={clickfunc}/>
+
+     </div>
+    
+ 
+
+    
   );
 }
 
